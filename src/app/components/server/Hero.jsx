63a52631ab2029a648b1614940ad46c6635721f4
Shuvodip1 <shuvodipduttabanik@gmail.com>
2024-02-07 20:02:37 +0530

    Image Uploader

--- src/app/components/server/Hero.jsx
+++ src/app/components/server/Hero.jsx
@@ -3,11 +3,10 @@ import React from 'react'
 const Hero = () => {
     return (
         <section className={`h-[90vh] flex items-center justify-start -mb-36`} id='hero'>
-            <div className="mx-24 px-3 py-0 flex md:flex-row flex-col items-center justify-start">
+            <div className="mx-24 px-3 py-0 flex md:flex-row flex-col items-center justify-start backdrop-blur-md">
                 <div className="flex justify-start flex-col items-start gap-[10px]">
-                    <h1 className="text-[#ab6031] text-5xl font-bold">
+                    <h1 className="text-5xl font-bold">
                         WELCOME TO HealthNest
-
                     </h1>
                     <p className="text-[#121b24] text-2xl">
                     Compassionate Care, Advanced Healing, Your Health is Our Mission.
